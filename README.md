1b8ac1b24f4fc64ed0eb50a7a57bb585f149b2f0
coder <coder@CA01LM005.local>
2022-02-18 10:23:30 -0500

    init commit

--- README.md
+++ README.md
@@ -1,2 +1,17 @@
 # upi_websdk_npm_demo
 npm demo project to test websdk node package
+
+use 
+npm install --save ../upi_ms_web_sdk
+
+to install the local package
+## getting started
+
+install dependencies and start local dev server
+
+```sh
+npm install
+npm run serve
+```
+
+## details
---
+++ src/views/Products.vue
@@ -0,0 +1,19 @@
+<template>
+	<main class="wrapper">
+			<div class="card-container">
+				<ProductCard v-for="(product, index) in inventory" :key="product.id" :index="index" :product="product" :addToCart="addToCart" class="card">
+        </ProductCard>
+			</div>
+	</main>
+</template>
+
+<script>
+import ProductCard from '@/components/ProductCard.vue'
+
+export default {
+	props: ['inventory', 'addToCart'],
+	components: {
+		ProductCard
+	}
+}
+</script>
